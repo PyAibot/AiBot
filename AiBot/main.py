2c6c852858b5253b389c52a7d86ddfef981eb4ea
chen <810975099@qq.com>
2022-07-25 10:05:56 +0800

    日志格式修改

--- AiBot/main.py
+++ AiBot/main.py
@@ -94,7 +94,7 @@ class AiBotMain(socketserver.BaseRequestHandler, metaclass=_protect("handle", "e
 
     log_path = ""
     log_level = "INFO"
-    log_format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | " + \
+    log_format = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | " + \
                  "<level>{level: <8}</level> | " + \
                  "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
 
