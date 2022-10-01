1c08fe275cef6482b4019a3a6fd7b65528ed2db1
Chen <810975099@qq.com>
2022-10-01 22:33:27 +0800

    新增接口

--- test/test.py
+++ test/test.py
@@ -8,11 +8,12 @@ class CustomScript(AiBotMain):
     def script_main(self):
         self.show_toast("连接成功")
         print(self.get_package())
-        self.create_text_view(100, "是否恢复理智", 1080, 1920, 0, 100, 400, 100)
-        self.create_check_box(100, "是否恢复理智", 1080, 1920, 0, 100, 400, 100)
-        self.create_exit_view(110, "10", 1080, 1920, 0, 250, 400, 150)
-        self.create_exit_view(120, "0", 1080, 1920, 0, 400, 400, 150)
+        self.create_check_box(100, "是否恢复理智", 1080, 1920, 0, 10, 400, 100)
+        self.create_exit_view(110, "药剂恢复次数", 1080, 1920, 0, 100, 400, 150)
+        self.create_exit_view(120, "源石恢复次数", 1080, 1920, 0, 250, 400, 150)
         params = self.get_script_params()
+        count = params.get("110")
+        count = params.get("110")
         count = params.get("110")
         print("理智恢复次数: ", count)
         while True:
