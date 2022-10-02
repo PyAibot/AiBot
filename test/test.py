9817f4037a2ea549255f4e2c4980feaabec0d4eb
Chen <810975099@qq.com>
2022-10-02 15:25:57 +0800

    创建控件接口精简参数

--- test/test.py
+++ test/test.py
@@ -12,8 +12,8 @@ class CustomScript(AiBotMain):
         self.create_check_box(110, "是否使用源石", 500, 120)
         params = self.get_script_params()
         restore_count = params.get("100")
-        print("理智恢复次数: ",  restore_count)
         is_use_ys = params.get("110")
+        print("理智恢复次数: ",  restore_count)
         print("是否使用源石: ",  is_use_ys)
         while True:
             time.sleep(5)
