ebb7feafa8def67763dae373d2e39f6df3ca34e5
Chen <810975099@qq.com>
2022-10-14 23:19:01 +0800

    demo

--- test/client.py
+++ test/client.py
@@ -1,22 +1,27 @@
 import socket
-import sys
+import time
 
-HOST = "localhost"  # The remote host
-PORT = 9999  # The same port as used by the server
+HOST = "localhost"
+PORT = 9999
 
 address_info = socket.getaddrinfo(HOST, PORT, socket.AF_INET, socket.SOCK_STREAM)[0]
 
 family, socket_type, proto, _, socket_address = address_info
 
-sock = socket.socket(family, socket_type, proto)
-sock.connect(socket_address)
+while True:
+    try:
+        sock = socket.socket(family, socket_type, proto)
+        sock.connect(socket_address)
 
-with sock:
-    while True:
-        data_ = input("请输入: \n")
+        with sock:
+            while True:
+                msg = "Hello World!"
+                n = sock.send(msg.encode("utf8"))
+                print(f'Send: {n} bytes', )
 
-        sock.sendall(data_.encode("utf8"))
-
-        data = sock.recv(1024)
-
-        print('Received: ', data.decode("utf8"))
+                time.sleep(1)
+                data = sock.recv(1024)
+                print(f'Received: ', data.decode("utf8"))
+                time.sleep(5)
+    except Exception as e:
+        print(e)
