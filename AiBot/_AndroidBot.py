4a28153cc536b09ebf259b31020aa4b44a7af842
Chen <810975099@qq.com>
2023-01-10 00:06:38 +0800

    AndroidBotMain: 1. 删除原 find_image 方法； 2. find_image_by_opencv 改名为 find_image；

--- AiBot/_AndroidBot.py
+++ AiBot/_AndroidBot.py
@@ -313,71 +313,7 @@ class AndroidBotMain(socketserver.BaseRequestHandler, metaclass=_protect("handle
     def find_image(self, image_name, region: _Region = None, algorithm: _Algorithm = None, similarity: float = 0.9,
                    wait_time: float = None, interval_time: float = None, raise_err: bool = None) -> Optional[_Point]:
         """
-        寻找图片坐标，在当前屏幕中寻找给定图片中心点的坐标，返回坐标或者 None
-        :param image_name: 图片名称（手机中）；
-        :param region: 从指定区域中找图，默认全屏；
-        :param algorithm: 处理屏幕截图所用的算法，默认原图，注意：给定图片处理时所用的算法，应该和此方法的算法一致；
-        :param similarity: 相似度，0-1 的浮点数，默认 0.9；
-        :param wait_time: 等待时间，默认取 self.wait_timeout；
-        :param interval_time: 轮询间隔时间，默认取 self.interval_timeout；
-        :param raise_err: 超时是否抛出异常；
-        :return:
-
-        # 区域相关参数
-        region = (0, 0, 0, 0) 按元素顺序分别代表：起点x、起点y、终点x、终点y，最终得到一个矩形。
-        # 算法相关参数
-        algorithm = (0, 0, 0) # 按元素顺序分别代表：algorithm_type 算法类型、threshold 阈值、max_val 最大值。
-        threshold 和 max_val 同为 255 时灰度处理.
-        0   THRESH_BINARY      算法，当前点值大于阈值 threshold 时，取最大值 max_val，否则设置为 0；
-        1   THRESH_BINARY_INV  算法，当前点值大于阈值 threshold 时，设置为 0，否则设置为最大值 max_val；
-        2   THRESH_TOZERO      算法，当前点值大于阈值 threshold 时，不改变，否则设置为 0；
-        3   THRESH_TOZERO_INV  算法，当前点值大于阈值 threshold 时，设置为 0，否则不改变；
-        4   THRESH_TRUNC       算法，当前点值大于阈值 threshold 时，设置为阈值 threshold，否则不改变；
-        5   ADAPTIVE_THRESH_MEAN_C      算法，自适应阈值；
-        6   ADAPTIVE_THRESH_GAUSSIAN_C  算法，自适应阈值；
-        """
-        if wait_time is None:
-            wait_time = self.wait_timeout
-
-        if interval_time is None:
-            interval_time = self.interval_timeout
-
-        if raise_err is None:
-            interval_time = self.raise_err
-
-        if not region:
-            region = [0, 0, 0, 0]
-
-        if not algorithm:
-            algorithm_type, threshold, max_val = [0, 0, 0]
-        else:
-            algorithm_type, threshold, max_val = algorithm
-            if algorithm_type in (5, 6):
-                threshold = 127
-                max_val = 255
-
-        end_time = time.time() + wait_time
-        while time.time() < end_time:
-            response = self.__send_data("findImage", self._base_path + image_name, *region, similarity,
-                                        algorithm_type, threshold, max_val)
-            # 找图失败
-            if response == "-1.0|-1.0":
-                time.sleep(interval_time)
-            else:
-                # 找图成功，返回图片左上角坐标
-                x, y = response.split("|")
-                return _Point(x=float(x), y=float(y), driver=self)
-        # 超时
-        if raise_err:
-            raise TimeoutError("`find_image` 操作超时。")
-        return None
-
-    def find_image_by_opencv(self, image_name, region: _Region = None, algorithm: _Algorithm = None,
-                             similarity: float = 0.9, wait_time: float = None, interval_time: float = None,
-                             raise_err: bool = None) -> Optional[_Point]:
-        """
         寻找图片坐标，在当前屏幕中寻找给定图片中心点的坐标，返回图片坐标或者 None
-        与 self.find_image() 基本一致，采用 OpenCV 算法
         :param image_name: 图片名称（手机中）；
         :param region: 从指定区域中找图，默认全屏；
         :param algorithm: 处理屏幕截图所用的算法，默认原图，注意：给定图片处理时所用的算法，应该和此方法的算法一致；
@@ -400,18 +336,16 @@ class AndroidBotMain(socketserver.BaseRequestHandler, metaclass=_protect("handle
         5   ADAPTIVE_THRESH_MEAN_C      算法，自适应阈值；
         6   ADAPTIVE_THRESH_GAUSSIAN_C  算法，自适应阈值；
         """
-        result = self.find_images_by_opencv(image_name, region, algorithm, similarity, 1, wait_time, interval_time,
-                                            raise_err)
+        result = self.find_images(image_name, region, algorithm, similarity, 1, wait_time, interval_time, raise_err)
         if not result:
             return None
         return result[0]
 
-    def find_images_by_opencv(self, image_name, region: _Region = None, algorithm: _Algorithm = None,
-                              similarity: float = 0.9, multi: int = 1, wait_time: float = None,
-                              interval_time: float = None, raise_err: bool = None) -> List[_Point]:
+    def find_images(self, image_name, region: _Region = None, algorithm: _Algorithm = None, similarity: float = 0.9,
+                    multi: int = 1, wait_time: float = None, interval_time: float = None,
+                    raise_err: bool = None) -> List[_Point]:
         """
         寻找图片坐标，在当前屏幕中寻找给定图片中心点的坐标，返回坐标列表
-        与 self.find_image() 基本一致，采用 OpenCV 算法，并且可找多个目标。
         :param image_name: 图片名称（手机中）；
         :param region: 从指定区域中找图，默认全屏；
         :param algorithm: 处理屏幕截图所用的算法，默认原图，注意：给定图片处理时所用的算法，应该和此方法的算法一致；
@@ -473,7 +407,7 @@ class AndroidBotMain(socketserver.BaseRequestHandler, metaclass=_protect("handle
                 return point_list
         # 超时
         if raise_err:
-            raise TimeoutError("`find_images_by_opencv` 操作超时。")
+            raise TimeoutError("`find_images` 操作超时。")
         return []
 
     def find_dynamic_image(self, interval_ti: int, region: _Region = None, wait_time: float = None,
