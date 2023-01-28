61e591a3eac64bda7fce2a14f16315ee5b5b6460
Chen <810975099@qq.com>
2023-01-28 18:20:36 +0800

    升级版本号

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
 
 setup_kwargs = {
     'name': 'AiBot.py',
-    'version': '1.1.2',
+    'version': '1.1.3',
     'description': '...',
     'long_description': '...',
     'long_description_content_type': 'text/markdown',
