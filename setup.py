43fe9337b7036125dc09acd46d55b387623883c5
chin <810975099@qq.com>
2023-08-17 20:51:24 +0800

    升级版本号到 1.3.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup_kwargs = {
     'name': 'AiBot.py',
-    'version': '1.2.14',
+    'version': '1.3.0',
     'description': '...',
     'long_description': '...',
     'long_description_content_type': 'text/markdown',
