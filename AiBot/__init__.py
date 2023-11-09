e70401d94ff3afa74e42315dacfe5edcaa5c96c1
Chen <810975099@qq.com>
2023-11-09 17:37:21 +0800

    升级版本号，加入最低支持 Python3.9的限制

--- AiBot/__init__.py
+++ AiBot/__init__.py
@@ -1,3 +1,8 @@
+import sys
+
+if sys.version_info < (3, 9):
+    raise RuntimeError("最低支持 Python3.9 版本，请升级！")
+
 from .AndroidBot import AndroidBotMain
 from .WebBot import WebBotMain
 from .WinBot import WinBotMain
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup_kwargs = {
     'name': 'AiBot.py',
-    'version': '1.3.3',
+    'version': '2.0.0',
     'description': '...',
     'long_description': '...',
     'long_description_content_type': 'text/markdown',
